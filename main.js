6bb8d6e41858f55c51d11ae3fa343f9d61ad442d
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-13 20:02:45 +0400

    practice 2 again

--- main.js
+++ main.js
@@ -9,15 +9,6 @@ const users = ["John", "Mark"];
 //Pulse 4
 app.use(express.json())
 
-//practice 2
-//a middleware that will log the request body if found
-/*
-app.use((req, res, next) => {
-    console.log(req.body.name)
-    next()
-
-})*/
-
 //Practice 1
 const router = express.Router();
 
@@ -59,10 +50,34 @@ app.use((req, res, next) => {
 const logMethod = (req, res, next) => {
     console.log(req.method);
     //dont use res in the first middlewares because it will prevent next() and will stop the cycle
-
     next();
 }
 app.use('/users', logMethod)
+
+//practice 2
+//a middleware that will log the request body if found
+app.use((req, res, next) => {
+    if (req.body.name) {
+        console.log(req.body.name);
+        next();
+    }
+    next();
+});
+
+app.post("/users/create", (req, res, next) => {
+    if (req.body.name) {
+        console.log("body is defined");
+        users.push(req.body.name);
+        console.log(users)
+        res.status = 201;
+        res.json("name added");
+    } else {
+        const err = new Error("no name entered");
+        err.status = 404
+        next(err);
+    };
+});
+
 
 //Pulse 5
 app.use((errrr, req, res, next) => {
@@ -70,28 +85,12 @@ app.use((errrr, req, res, next) => {
     //since you passed it from the request API
     res.status = errrr.status
     res.json({
-            error: {
-                status: errrr.status,
-                message: errrr.message
-            }
-        })
-        //no need for next because this is a dead end and will show response on client side
-})
-
-//Practice 2
-app.post("/users/create", (req, res, next) => {
-    if (req.body.name) {
-        console.log("body is defined")
-        res.status = 201
-        users.push(req.body.name)
-            // console.log(users)
-        res.json("name added")
-    } else {
-        console.log("body is not defined")
-
-        res.status = 404
-        res.json("please enter a name")
-    }
+        error: {
+            status: errrr.status,
+            message: errrr.message
+        }
+    });
+    //no need for next because this is a dead end and will show response on client side
 });
 
 
